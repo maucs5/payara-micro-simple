6042905164e6a1ce4ca946b515abdef33555f885
Mau <comments@maucs.com>
2019-04-11 17:05:30 +0800

    Vagrant typo, and test @Clustered on Domain Data Grid

--- src/main/java/app/RandomGenerator.java
+++ src/main/java/app/RandomGenerator.java
@@ -1,8 +1,11 @@
 package app;
 
+import fish.payara.cluster.Clustered;
+
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Random;
 
+@Clustered
 @ApplicationScoped
 public class RandomGenerator {
     private final int number = new Random().nextInt();
