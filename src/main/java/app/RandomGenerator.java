a6e8b4ed3c71e88c5f00b4857224db9195c1dacb
Mau <comments@maucs.com>
2019-04-11 17:07:35 +0800

    Implements Serializable on Clustered scope

--- src/main/java/app/RandomGenerator.java
+++ src/main/java/app/RandomGenerator.java
@@ -3,11 +3,12 @@ package app;
 import fish.payara.cluster.Clustered;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.io.Serializable;
 import java.util.Random;
 
 @Clustered
 @ApplicationScoped
-public class RandomGenerator {
+public class RandomGenerator implements Serializable {
     private final int number = new Random().nextInt();
 
     int getNumber() {
